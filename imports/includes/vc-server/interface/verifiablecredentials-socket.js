40bed0fc340533d37d3371d4a652cdb88cc42652
fbaud <fbaud@p2pventure.org>
2024-03-13 14:09:17 +0100

    version 0.30.40

--- imports/includes/vc-server/interface/verifiablecredentials-socket.js
+++ imports/includes/vc-server/interface/verifiablecredentials-socket.js
@@ -1,54 +1,3 @@
-'use strict';
-
-class ClientWebSocket {
-	constructor(sessionuuid, connectionuuid, server_socket, socket_connection) {
-		this.sessionuuid = sessionuuid;
-		this.connectionuuid = connectionuuid;
-
-		this.server_socket = server_socket;
-		this.socket_connection = socket_connection;
-
-
-		this.socket_connection.socket_connection.onclose = (event) => {
-			this._onClose(event);
-		}
-
-		this.socket_connection.socket_connection.onerror = (event) => {
-			this._onError(event);
-		}
-
-		/* this.socket_connection.onclose = (event) => {
-			this._onClose(event);
-		}
-
-		this.socket_connection.onerror = (event) => {
-			this._onError(event);
-		} */
-	}
-
-	_onClose(event) {
-		console.log('closing websocket for session ' + this.sessionuuid + ' and connection ' + this.connectionuuid);
-		console.log('event is: ' + JSON.stringify(event));
-
-		// chain to vc connect client socket
-		if (this.socket_connection) this.socket_connection._onClose(event);
-
-		this.socket_connection = null;
-
-		let json = {sessionuuid: this.sessionuuid, connectuuid: this.connectionuuid};
-		this.server_socket._dispatchActionEvent('onConnectionClosed', json);
-	}
-	
-	_onError(event) {
-		console.log('error on websocket for session ' + this.sessionuuid + ' and connection ' + this.connectionuuid);
-
-		// chain to vc connect client socket
-		if (this.socket_connection) this.socket_connection._onError(event);
-
-		this.socket_connection = null;
-	}
-}
-
 var VerifiableCredentialsServerSocket = class {
 	
 	constructor(session) {
@@ -61,6 +10,13 @@ var VerifiableCredentialsServerSocket = class {
 		this.client_websocket = null;
 
 		this.action_listeners = {};
+
+		this.request_responders = {};
+	}
+
+	_getVcConnectClass() {
+		const VcConnect = require('../../vc-connect-client');
+		return VcConnect;
 	}
 
 	isConnected() {
@@ -93,6 +49,8 @@ var VerifiableCredentialsServerSocket = class {
 		}
 	}
 
+	//
+	// actions
 	addActionListener(action, func, uuid) {
 		this.action_listeners[action + (uuid ? '_' + uuid : this.session.guid())] = func;
 	}
@@ -109,21 +67,50 @@ var VerifiableCredentialsServerSocket = class {
 
 	_dispatchActionEvent(action, json) {
 		let keys = Object.keys(this.action_listeners);
+
+		switch(action) {
+			case 'onReceiveServerRequest': {
+				this.onReceiveServerRequest(json).catch(err => {});
+			}
+			break;
+
+			default: {
+				for (var i = 0; i < keys.length; i++) {
+					let key = keys[i];
 		
-		for (var i = 0; i < keys.length; i++) {
-			let key = keys[i];
-
-			if (key.startsWith(action)) {
-				let func = this.action_listeners[key];
-
-				try {
-					func(json);
-				}
-				catch(e) {
+					if (key.startsWith(action)) {
+						let func = this.action_listeners[key];
+		
+						try {
+							func(json);
+						}
+						catch(e) {
+						}
+					}
 				}
 			}
+			break;
+		}
+		
+
+	}
+
+	//
+	// requests
+	addRequestResponder(obj, uuid) {
+		this.request_responders[(uuid ? '_' + uuid : this.session.guid())] = obj;
+	}
+
+	removeRequestResponder(uuid) {
+		if (!uuid) return;
+
+		let obj = this.request_responders[(uuid ? '_' + uuid : '')];
+
+		if (obj) {
+			delete this.request_responders[(uuid ? '_' + uuid : '')];
 		}
 	}
+
 
 	async _getWebSocketServer() {
 		if (this.websocket_server)
@@ -134,7 +121,7 @@ var VerifiableCredentialsServerSocket = class {
 
 		var server_url = wss_server_url + (wss_server_api_path ? wss_server_api_path : '');
 
-		const VcConnect = require('../../vc-connect-client');
+		const VcConnect = this._getVcConnectClass();
 		const WebSocketServer = VcConnect.WebSocketServer;
 
 		this.websocket_server = WebSocketServer.getObject(wss_server_url, wss_server_api_path);
@@ -149,9 +136,9 @@ var VerifiableCredentialsServerSocket = class {
 		let websocket_server = await this._getWebSocketServer();
 		
 		// create web socket
-		var session = this.session;
-		var sessionuuid = session.getSessionUUID();
-		var connectionuuid = session.guid();
+		let session = this.session;
+		let sessionuuid = session.getSessionUUID();
+		let connectionuuid = session.guid();
 
 		let vc_connect_client_socket = await websocket_server.createClientWebSocket(sessionuuid, connectionuuid, null, this._onMessage.bind(this));
 						// re-route messages to our method (and we chain messages in it)
@@ -213,12 +200,164 @@ var VerifiableCredentialsServerSocket = class {
 		return this.postPacket({}, data);
 	}
 
-	// actions
+	// notifications sent to remote paired client
+	// (e.g. wallet to widget, or widget to wallet)
+	// it is using the VcConnect mechanisms and NOT
+	// the VerifiableCredentialsServerSocket layer
+	async notifyRemoteClient(remote_connectionuuid, notification) {
+		const VcConnect = this._getVcConnectClass();
+		const RemotePairCalls = VcConnect.RemotePairCalls;
+
+		let action = notification.action;
+		let params = notification.params;
+
+		if (!params.remote_connectionuuid) params.remote_connectionuuid = remote_connectionuuid;
+
+		let session = this.session;
+		let sessionuuid = session.getSessionUUID();
+
+		let rpc = new RemotePairCalls(this.client_websocket, sessionuuid, remote_connectionuuid);
+
+		await rpc.init();
+
+		let result = await rpc.sendNotification(action, params);
+		
+		await rpc.close();
+
+		return result;
+	}
+
+	async requestRemoteClient(remote_connectionuuid, request) {
+		const VcConnect = this._getVcConnectClass();
+		const RemotePairCalls = VcConnect.RemotePairCalls;
+
+		let action = request.action;
+		let params = request.params;
+
+		if (!params.remote_connectionuuid) params.remote_connectionuuid = remote_connectionuuid;
+
+		let session = this.session;
+		let sessionuuid = session.getSessionUUID();
+
+		let rpc = new RemotePairCalls(this.client_websocket, sessionuuid, remote_connectionuuid);
+
+		await rpc.init();
+
+		let result = await rpc.invokeCall(action, params);
+		
+		await rpc.close();
+
+		return result;
+	}
+
+	// answering server requests
+	async onReceiveServerRequest(json) {
+		let answer = {status: 'reject'};
+
+		// dispatch to objects that want to handle
+		// the action by themselves
+		let keys = Object.keys(this.action_listeners);
+		let handled = false;
+
+		for (var i = 0; i < keys.length; i++) {
+			let key = keys[i];
+	
+			if (key.startsWith('onReceiveServerRequest')) {
+				let func = this.action_listeners[key];
+	
+				try {
+					func(json);
+					handled = true;
+					answer.status = 'answered';
+					break;
+				}
+				catch(e) {
+				}
+			}
+		}
+
+		// get list of objects that can respond to 
+		// specific server requests
+		if (handled === false) {
+			let data = (json.data ? json.data : {});
+			let calluuid = data.calluuid;
+	
+			let request = (data.request ? data.request : {});
+			let question = request.question;
+			let params = request.params;
+
+			let arr = Object.values(this.request_responders);
+
+			for (var i = 0; i < arr.length; i++) {
+				let obj = arr[i];
+		
+				if (question && obj[question]) {
+					let func = obj[question].bind(obj);
+		
+					try {
+						answer = await func(params);
+						handled = true;
+						answer.status = 'accept';
+						break;
+					}
+					catch(e) {
+					}
+				}
+			}
+
+		}
+
+
+		if ((answer.status === 'reject')  || (answer.status === 'accept')) {
+			await this.answerServerRequest(json, answer);
+		}
+
+	}
+
+	async answerServerRequest(json, answer) {
+		let calluuid = (json.data ? json.data.calluuid : null);
+
+		let session = this.session;
+		let sessionuuid = session.getSessionUUID();
+		let connectionuuid = (json.header ? json.header.connectionuuid : null);
+
+		let header = {action: 'receiveClientAnswer', sessionuuid, connectionuuid};
+		let data = {params: {calluuid, answer}};
+
+		return this.postPacket(header, data);
+	}
+	
+
+	// notifications sent to server
+	// FOR TEST PURPOSES use REST calls to 
+	// use REST calls to start communications with server
+	async notifyServer(notification) {
+		const VcConnect = this._getVcConnectClass();
+		const RemotePairCalls = VcConnect.RemotePairCalls;
+
+		let action = notification.action;
+		let params = notification.params;
+
+		let header = {action};
+		let data = params;
+
+		await this.postPacket(header, data);
+	}
+
+	async nonce_changed(nonce) {
+
+		let notification = {action: 'nonce_changed',params: {nonce}};
+
+		await this.notifyServer(notification);
+
+		console.log('nonce_changed');
+	}
+	
 	async change_did(from, to) {
 
 		var header = {action: 'change_did'}
 
-		var data = {}
+		var data = {};
 		data.from = from;
 		data.to = to;
 
@@ -226,6 +365,9 @@ var VerifiableCredentialsServerSocket = class {
 
 		console.log('change_did');
 	}
+
+
+
 }
 
 
